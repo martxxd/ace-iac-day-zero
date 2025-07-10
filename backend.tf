b45d6e14364e9949617d082f166c6b9eff93b3cb
Remart Abejay <152456545+martxxd@users.noreply.github.com>
2025-07-10 22:24:04 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "ace-training-remart"
+    organization = "ace-training-remart"
     workspaces {
       name = "ace-iac-day-zero"
     }
