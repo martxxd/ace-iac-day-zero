5f92848875552d8b423120e5cf1d80cb318932b0
Remart Abejay <152456545+martxxd@users.noreply.github.com>
2025-07-11 17:54:00 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
     source  = "AviatrixSystems/aviatrix"
-    version = ">= 3.1.6, < 3.2.0"
+    version = "~> 3.0.1"
 }
     aws = {
       source  = "hashicorp/aws"
